d5f41571e2b43df7170e98ea8b93fffa323f9f53
Juno <doplitog@gmail.com>
2026-02-04 20:49:09 -0800

    add commands handler and registry

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,55 @@
-import { setUser, readConfig } from "./config";
+import { setUser } from "./config";
+
+type CommandHandler = (cmdName: string, ...args: string[]) => void;
+type CommandsRegistry = Record<string, CommandHandler>;
+
+function registerCommand(
+  registry: CommandsRegistry,
+  cmdName: string,
+  handler: CommandHandler,
+) {
+  registry[cmdName] = handler;
+}
+
+function runCommand(
+  registry: CommandsRegistry,
+  cmdName: string,
+  ...args: string[]
+) {
+  const handler = registry[cmdName];
+  if (!handler) {
+    throw new Error(`Unknown command: ${cmdName}`);
+  }
+  handler(cmdName, ...args);
+}
+
+function handlerLogin(cmdName: string, ...args: string[]) {
+  if (args.length === 0) {
+    throw new Error("username is required");
+  }
+  setUser(args[0]);
+  console.log(`User has been set to ${args[0]}`);
+}
 
 function main() {
-  setUser("Juno");
-  const config = readConfig();
-  console.log(config);
+  const registry: CommandsRegistry = {};
+  registerCommand(registry, "login", handlerLogin);
+
+  const args = process.argv.slice(2);
+  if (args.length === 0) {
+    console.error("not enough arguments");
+    process.exit(1);
+  }
+
+  const [cmdName, ...cmdArgs] = args;
+  try {
+    runCommand(registry, cmdName, ...cmdArgs);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error(err.message);
+    }
+    process.exit(1);
+  }
 }
 
 main();
